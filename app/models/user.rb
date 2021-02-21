61a9d3119c6e96e556f761b5c23f9acd17d1db70
yo108039 <y_obara127@yahoo.co.jp>
2021-02-21 11:19:34 +0900

    ユーザー管理機能(コーディング)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nick_name, presence: true
+  validates :last_name, presence: true
+  validates :name, presence: true
+  validates :last_name_kana, presence: true
+  validates :name_kana, presence: true
+  validates :birth_day, presence: true
+
+  has_many :items
+  has_many :purchases
+
 end
