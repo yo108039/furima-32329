b118eb78f0770eeca1b341ffcb77e1975e457f8f
yo108039 <y_obara127@yahoo.co.jp>
2021-02-23 14:29:31 +0900

    ユーザー管理機能(レビュー後修正 - 3回目)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,20 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true do
+    
     validates :nick_name
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください'}
-    validates :name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください'}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: 'は全角カナを使用してください'}
-    validates :name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: 'は全角カナを使用してください'}
     validates :birth_day
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください'} do
+      validates :last_name
+      validates :name
+    end
+
+    with_options format: {with: /\A[ァ-ヶ]+\z/, message: 'は全角カナを使用してください'} do
+      validates :last_name_kana 
+      validates :name_kana
+    end
+
   end
 
   has_many :items
