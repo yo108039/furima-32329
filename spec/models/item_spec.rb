51217e87744ce6b8e3bc7f4235e1971e89705c3c
yo108039 <y_obara127@yahoo.co.jp>
2021-02-27 18:35:31 +0900

    商品出品機能(レビュー後修正 2回目)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -104,13 +104,13 @@ RSpec.describe Item, type: :model do
       end
 
       it '価格の範囲が¥300未満の場合、登録できない' do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
       end
 
       it '価格の範囲が¥10,000,000以上の場合、登録できない' do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
       end
