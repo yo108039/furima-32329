54c6d20d645e687bf9aa908f31a784219ae842d7
yo108039 <y_obara127@yahoo.co.jp>
2021-02-28 12:25:31 +0900

    商品一覧表示機能(レビュー後修正 2回目)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @item = Item.all.order("created_at DESC")
+    @item = Item.includes(:user).order("created_at DESC")
+    binding.pry
   end
 
   def new
