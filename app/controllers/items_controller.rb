2066139b88479cd3ae33e1bbc7129999bcd01565
yo108039 <y_obara127@yahoo.co.jp>
2021-03-23 21:37:45 +0900

    商品購入機能(レビュー後修正 2回目)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :current_user_discrimination, only: [:edit, :update, :destroy]
+  before_action :judge_item, only: [:edit, :update, :destroy]
 
   def index
     @item = Item.includes(:user).order("created_at DESC")
@@ -50,6 +51,13 @@ class ItemsController < ApplicationController
 
   def current_user_discrimination
     if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  #購入履歴判断処理
+  def judge_item
+    if @item.order.nil? != true
       redirect_to root_path
     end
   end
